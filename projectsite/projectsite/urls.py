bafc239aeaf73442da63c6b3fcff985329eebe36
Irish jean Gillona <irishjeangillona@gmail.com>
2023-12-09 22:28:37 +0800

    updating files

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,11 +16,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from cardquest.views import HomePageView, TrainerList
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+from django.urls import path
 from cardquest import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.HomePageView.as_view(), name='home'),
-    path('trainer_list', TrainerList.as_view(), name='trainer-list'),
+    path('',views.HomePageView.as_view(),name='home'),
+]
+
+from cardquest.views import HomePageView, TrainerList, PokemonCardList ,CollectionList
+
+urlpatterns = [
+path('admin/', admin.site.urls), 
+path('', views.HomePageView.as_view(), name='home'),
+path('trainer_list/', TrainerList.as_view(), name='trainer-list'), 
+path('pokemon-card-list/', PokemonCardList.as_view(), name='pokemon-card-list'),
+path('collections/', CollectionList.as_view(), name='collection-list'),
 ]
